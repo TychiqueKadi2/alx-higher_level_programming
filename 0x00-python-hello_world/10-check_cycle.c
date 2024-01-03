a95adc4fb73104d0557003abb3d82c3b72e020c8
TychiqueKadi2 <tychiquemukadi@gmail.com>
2024-01-02 17:44:05 -0800

    python hello world advanced

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a linked list has a cycle
+ * @list: linked list
+ * Return: 1 if cycle true, else 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+	if (!list)
+		return (0);
+
+	while(slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
