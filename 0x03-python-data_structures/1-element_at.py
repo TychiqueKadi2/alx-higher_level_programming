d887a235b0aab5a5a8aed5f6c78555d8dcd9f905
TychiqueKadi2 <tychiquemukadi@gmail.com>
2024-01-05 02:20:58 -0800

    function that replaces an element of a list at a specific position

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -7,3 +7,9 @@ def element_at(my_list, idx):
             return (None)
         else:
             return(idx + 1)
+
+    """if idx < 0:
+        return (None)
+    if idx len(my_list) -1:
+        return (None)
+    return (my_list[idx])"""
